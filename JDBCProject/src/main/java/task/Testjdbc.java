900168c22428dd782e093295a9580aa118283946
Majidova <89316143+Majid0va@users.noreply.github.com>
2022-11-08 22:04:11 +0400

    Update Testjdbc.java

--- JDBCProject/src/main/java/task/Testjdbc.java
+++ JDBCProject/src/main/java/task/Testjdbc.java
@@ -3,7 +3,7 @@ package task;
 public class Testjdbc {
     public static void main(String[] args) {
         Employees employees = new Employees();
-        employees.setEmployeeId(41);
+        employees.setEmployeeId(208L);
         employees.setFirstName("Kate");
         employees.setLastName("Johnson");
         employees.setEmail("kate.jhons@sqltutorial.org");
@@ -14,14 +14,27 @@ public class Testjdbc {
         employees.setManagerId(101);
         employees.setDepartmentId(11);
 
+        Employees employees2 = new Employees();
+        employees2.setEmployeeId(43L);
+        employees.setFirstName("Jhon");
+        employees.setLastName("Johnson");
+        employees.setEmail("kate.jhons@sqltutorial.org");
+        employees.setPhoneNumber("515.123.1212");
+        employees.setHireDate("1998-08-11");
+        employees.setJobId(19);
+        employees.setSalary(8700);
+        employees.setManagerId(101);
+        employees.setDepartmentId(11);
+
+        //Tasks
         Methods.printStatements();
 
 //        Methods.insertNewEmployee(employees);
 
 //        Methods.insertNewDepartmentsBatch();
 
-//        Methods.updateEmployeeId(employees.getFirstName(), employees.getEmployeeId());
+//        Methods.updateEmployeeId(employees2.getFirstName(), employees2.getEmployeeId());
 
-//        Methods.deleteEmployeeWithId(41L);
+//        Methods.deleteEmployeeWithId(208L);
     }
 }
